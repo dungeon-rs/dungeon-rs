5ccd8d6fe5f151a87410b5de7502e538c3eedecf
Wannes Gennar <wannes.gennar@gmail.com>
2025-04-26 22:38:57 +0200

    setup Bevy
    added minimal Bevy setup
    added dev featureset
    added basic recommended optimizations

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use bevy::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_systems(Startup, add_camera)
+        .run();
+}
+
+fn add_camera(mut commands: Commands) {
+    commands.spawn(Camera2d);
 }
