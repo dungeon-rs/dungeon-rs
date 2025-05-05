21a8228e98cfd6e34412872f50340f31fc2a3296
Wannes Gennar <wannes.gennar@gmail.com>
2025-05-05 14:25:50 +0200

    minor POC for inspector values

--- packages/ui/src/lib.rs
+++ packages/ui/src/lib.rs
@@ -1,5 +1,6 @@
 use bevy::prelude::*;
 use bevy_egui::{EguiContextPass, EguiContexts, EguiPlugin, egui};
+use bevy_inspector_egui::{DefaultInspectorConfigPlugin, reflect_inspector};
 
 #[derive(Default)]
 pub struct UIPlugin;
@@ -9,12 +10,31 @@ impl Plugin for UIPlugin {
         app.add_plugins(EguiPlugin {
             enable_multipass_for_primary_context: true,
         })
+        .add_plugins(DefaultInspectorConfigPlugin)
         .add_systems(EguiContextPass, ui_example_system);
     }
 }
 
-fn ui_example_system(mut contexts: EguiContexts) {
-    egui::Window::new("Hello").show(contexts.ctx_mut(), |ui| {
-        ui.label("world");
-    });
+fn ui_example_system(
+    mut contexts: EguiContexts,
+    registry: Res<AppTypeRegistry>,
+    mut backcolor: ResMut<ClearColor>,
+) {
+    let Some(context) = contexts.try_ctx_mut() else {
+        return;
+    };
+    let registry = registry.read();
+
+    egui::SidePanel::right("inspector_panel")
+        .resizable(true)
+        .show(context, |ui| {
+            egui::CollapsingHeader::new("Colours").show(ui, |ui| {
+                for _ in 1..10 {
+                    ui.horizontal(|ui| {
+                        ui.label("Background color");
+                        reflect_inspector::ui_for_value(&mut backcolor.0, ui, &registry);
+                    });
+                }
+            })
+        });
 }
