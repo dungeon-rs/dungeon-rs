6c73e573d9b87b99a7d618292698fccb7ee68480
Wannes Gennar <wannes.gennar@gmail.com>
2025-05-06 13:34:20 +0200

    add functionality for picking sprites and modifying basic properties

--- packages/ui/src/lib.rs
+++ packages/ui/src/lib.rs
@@ -3,7 +3,7 @@ mod widgets;
 use crate::widgets::toolbar::toolbar;
 use bevy::prelude::*;
 use bevy_egui::{EguiContextPass, EguiContexts, EguiPlugin};
-use bevy_inspector_egui::DefaultInspectorConfigPlugin;
+use bevy_inspector_egui::{DefaultInspectorConfigPlugin, reflect_inspector};
 
 /// The UI plugin handles registering and setting up all user interface elements of the editor.
 /// This module is unavailable in headless mode and should not contain any actual functionality,
@@ -11,17 +11,48 @@ use bevy_inspector_egui::DefaultInspectorConfigPlugin;
 #[derive(Default)]
 pub struct UIPlugin;
 
+#[derive(Resource)]
+struct EntityToInspect {
+    entity: Entity,
+}
+
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
         app.add_plugins(EguiPlugin {
             enable_multipass_for_primary_context: true,
         })
         .add_plugins(DefaultInspectorConfigPlugin)
+        .add_systems(Startup, setup)
         .add_systems(EguiContextPass, editor_interface);
     }
 }
 
-fn editor_interface(mut contexts: EguiContexts) {
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands
+        .spawn((
+            Pickable::default(),
+            Name::from("left"),
+            Sprite::from_image(asset_server.load("example.png")),
+            Transform::from_xyz(-100.0, 0.0, 0.0),
+        ))
+        .observe(on_click_sprite);
+
+    commands
+        .spawn((
+            Pickable::default(),
+            Name::from("right"),
+            Sprite::from_image(asset_server.load("example.png")),
+            Transform::from_xyz(100.0, 0.0, 0.0),
+        ))
+        .observe(on_click_sprite);
+}
+
+fn editor_interface(
+    mut contexts: EguiContexts,
+    entity: Option<Res<EntityToInspect>>,
+    mut query: Query<&mut Transform>,
+    registry: Res<AppTypeRegistry>,
+) {
     let Some(context) = contexts.try_ctx_mut() else {
         return;
     };
@@ -32,5 +63,28 @@ fn editor_interface(mut contexts: EguiContexts) {
         .resizable(true)
         .show(context, |ui| {
             //         side_panel.ui(ui, &mut Viewer);
+            if let Some(entity) = entity {
+                let registry = registry.read();
+                let mut transform = query.get_mut(entity.entity).unwrap();
+
+                reflect_inspector::ui_for_value(&mut transform.translation, ui, &*registry);
+                reflect_inspector::ui_for_value(&mut transform.rotation, ui, &*registry);
+                reflect_inspector::ui_for_value(&mut transform.scale, ui, &*registry);
+            }
         });
+}
+
+fn on_click_sprite(
+    event: Trigger<Pointer<Pressed>>,
+    mut commands: Commands,
+    mut query: Query<(&Name, &mut Transform)>,
+) {
+    let Ok((name, transform)) = query.get_mut(event.target) else {
+        return;
+    };
+
+    info!("Clicked on {}", name);
+    commands.insert_resource(EntityToInspect {
+        entity: event.target,
+    });
 }
