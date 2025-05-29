6bf0edeff625ca7139e7db3bf6a1926944b7d926
Wannes Gennar <wannes.gennar@gmail.com>
2025-05-29 15:31:51 +0300

    [data] add `Project`, `Level` and `Layer` structs

---
+++ crates/data/src/layer.rs
@@ -0,0 +1,50 @@
+use bevy::prelude::{Bundle, Component, Name, Transform, Visibility};
+use std::borrow::Cow;
+
+/// A [`Layer`] represents a distinct editing plane within a [`crate::Level`], allowing elements to be
+/// grouped by function or intent (e.g., "Walls", "Objects", "Lighting").
+///
+/// Layers are most commonly used for selective visibility, per-layer export, and to control edit
+/// interactions (such as locking or isolating layers during drawing or object placement).
+#[derive(Component, Default)]
+#[component(immutable)]
+#[require(Transform::from_xyz(0.0, 0.0, 0.0), Visibility::default())]
+pub struct Layer;
+
+impl Layer {
+    /// Generates a new [`Bundle`] with a layer under which graphic items can be grouped.
+    ///
+    /// # Examples
+    ///
+    /// Here's how to spawn a simple `Layer` for the Walls.
+    ///
+    /// ```
+    /// # use bevy::prelude::*;
+    /// # use data::Project;
+    /// # use data::Level;
+    /// # use data::Layer;
+    /// #
+    /// # fn main() {
+    /// #   App::new()
+    /// #       .add_systems(Startup, spawn_project)
+    /// #       .run();
+    /// # }
+    /// #
+    /// # fn spawn_project(mut commands: Commands) {
+    ///     commands.spawn((
+    ///         Project::new("Roadside Inn"),
+    ///         children![
+    ///             Level::new("Ground Floor"),
+    ///             children![
+    ///                 Layer::new("Walls"),
+    ///             ]
+    ///         ]
+    ///     ));
+    /// # }
+    /// ```
+    #[allow(clippy::new_ret_no_self)]
+    #[must_use]
+    pub fn new(name: impl Into<Cow<'static, str>>) -> impl Bundle {
+        (Name::new(name), Layer {})
+    }
+}
--- crates/data/src/lib.rs
+++ crates/data/src/lib.rs
@@ -1,2 +1,10 @@
 #![doc = include_str!("../README.md")]
 #![warn(clippy::pedantic, clippy::suspicious, clippy::complexity)]
+
+mod layer;
+mod level;
+mod project;
+
+pub use layer::*;
+pub use level::*;
+pub use project::*;
