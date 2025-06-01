d87906e97bbf2b061bede4076f0aaf44b008888c
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-01 11:48:45 +0200

    [editor, ui] add image features and layer POC

--- crates/ui/src/plugin.rs
+++ crates/ui/src/plugin.rs
@@ -1,9 +1,13 @@
 use bevy::app::App;
-use bevy::prelude::Plugin;
+use bevy::prelude::{AssetServer, Camera2d, Commands, Plugin, ResMut, Sprite, Startup, Transform};
+use bevy::render::view::RenderLayers;
 use bevy_egui::{EguiContextPass, EguiContexts, EguiPlugin, egui};
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 pub struct UIPlugin;
+
+const LAYER_A: usize = 1;
+const LAYER_B: usize = 2;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
@@ -11,8 +15,24 @@ impl Plugin for UIPlugin {
             enable_multipass_for_primary_context: true,
         })
         .add_plugins(WorldInspectorPlugin::new())
-        .add_systems(EguiContextPass, window);
+        .add_systems(EguiContextPass, window)
+        .add_systems(Startup, setup);
     }
+}
+
+pub fn setup(mut commands: Commands, asset_server: ResMut<AssetServer>) {
+    commands.spawn((Camera2d, RenderLayers::layer(LAYER_A)));
+
+    commands.spawn((
+        Sprite::from_image(asset_server.load("logo.png")),
+        Transform::from_xyz(250.0, 250.0, 0.0),
+        RenderLayers::layer(LAYER_A),
+    ));
+
+    commands.spawn((
+        Sprite::from_image(asset_server.load("logo.png")),
+        RenderLayers::layer(LAYER_B),
+    ));
 }
 
 pub fn window(mut contexts: EguiContexts) {
