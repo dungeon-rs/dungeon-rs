a7e2475cce9f22d1689d557ef5faa5f23b2b09e6
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-03 09:12:38 +0200

    [serialization] add serialize unit tests (incomplete)
    [skip ci]

---
+++ crates/serialization/tests/serialize.rs
@@ -0,0 +1,39 @@
+#![cfg(test)]
+#![allow(missing_docs)]
+#![allow(clippy::pedantic)]
+
+use serialization::{SerializationFormat, serialize};
+
+#[derive(serde::Serialize, serde::Deserialize)]
+pub struct Foo {
+    pub bar: String,
+    pub baz: u32,
+}
+
+#[test]
+fn serialize_json() {
+    let foo = Foo {
+        bar: "bar".to_string(),
+        baz: 42,
+    };
+
+    let json = serialize(&foo, &SerializationFormat::JSON).unwrap();
+    assert_eq!(
+        json,
+        br#"{
+  "bar": "bar",
+  "baz": 42
+}"#
+    );
+}
+
+#[test]
+fn serialize_messagepack() {
+    let foo = Foo {
+        bar: "bar".to_string(),
+        baz: 42,
+    };
+
+    let msgpack = serialize(&foo, &SerializationFormat::MessagePack).unwrap();
+    assert_eq!(msgpack, Vec::<u8>::from([146, 163, 98, 97, 114, 42]));
+}
