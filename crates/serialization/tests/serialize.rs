f131a371ddd38fb537a0206953f17e399c76cbe7
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-03 10:39:17 +0200

    [serialization] add deserialize unit tests and complete serialize tests

--- crates/serialization/tests/serialize.rs
+++ crates/serialization/tests/serialize.rs
@@ -37,3 +37,19 @@ fn serialize_messagepack() {
     let msgpack = serialize(&foo, &SerializationFormat::MessagePack).unwrap();
     assert_eq!(msgpack, Vec::<u8>::from([146, 163, 98, 97, 114, 42]));
 }
+
+#[test]
+fn serialize_toml() {
+    let foo = Foo {
+        bar: "bar".to_string(),
+        baz: 42,
+    };
+
+    let toml = serialize(&foo, &SerializationFormat::Toml).unwrap();
+    assert_eq!(
+        toml,
+        br#"bar = "bar"
+baz = 42
+"#
+    );
+}
