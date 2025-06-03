6dfac79922c4b1116681d1ebb3037238637a04c1
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-03 17:05:39 +0200

    [core] add methods for each of Bevy's task pools

--- crates/core/src/async_ecs.rs
+++ crates/core/src/async_ecs.rs
@@ -6,7 +6,7 @@
 use bevy::ecs::world::CommandQueue;
 use bevy::prelude::{BevyError, Commands, Component, Entity, Query};
 use bevy::tasks::futures_lite::future;
-use bevy::tasks::{AsyncComputeTaskPool, Task, block_on};
+use bevy::tasks::{AsyncComputeTaskPool, ComputeTaskPool, IoTaskPool, Task, block_on};
 use crossbeam_channel::unbounded;
 pub use crossbeam_channel::{Receiver, Sender};
 use std::future::Future;
@@ -23,18 +23,53 @@ pub struct AsyncComponent {
 }
 
 impl AsyncComponent {
-    /// Generates a new [`AsyncComponent`] for the `task`.
+    /// Generates a new [`AsyncComponent`] for the `task`, scheduled on the [`AsyncComputeTaskPool`].
+    /// Like the [`AsyncComputeTaskPool`], this is intended for CPU-intensive work that may span
+    /// across multiple frames.
     ///
     /// Given an `async` function that takes a `Sender` this method schedules it on Bevy's
-    /// `AsyncComputeTaskPool` and handles polling progress and completion on the main schedule.
+    /// `TaskPool` and handles polling progress and completion on the main schedule.
     #[must_use = "async components do nothing unless polled"]
-    pub fn new<F, Fut>(task: F) -> Self
+    pub fn new_async<F, Fut>(task: F) -> Self
     where
         F: FnOnce(Sender<CommandQueue>) -> Fut + Send + 'static,
         Fut: Future<Output = Result<(), BevyError>> + Send + 'static,
     {
         let (sender, receiver) = unbounded::<CommandQueue>();
         let task = AsyncComputeTaskPool::get().spawn(task(sender));
+        AsyncComponent { task, receiver }
+    }
+
+    /// Generates a new [`AsyncComponent`] for the `task`, scheduled on the [`ComputeTaskPool`].
+    /// Like the [`ComputeTaskPool`], this is intended for CPU-intensive work that must be completed
+    /// to deliver the next frame.
+    ///
+    /// Given an `async` function that takes a `Sender` this method schedules it on Bevy's
+    /// `TaskPool` and handles polling progress and completion on the main schedule.
+    #[must_use = "async components do nothing unless polled"]
+    pub fn new_compute<F, Fut>(task: F) -> Self
+    where
+        F: FnOnce(Sender<CommandQueue>) -> Fut + Send + 'static,
+        Fut: Future<Output = Result<(), BevyError>> + Send + 'static,
+    {
+        let (sender, receiver) = unbounded::<CommandQueue>();
+        let task = ComputeTaskPool::get().spawn(task(sender));
+        AsyncComponent { task, receiver }
+    }
+
+    /// Generates a new [`AsyncComponent`] for the `task`, scheduled on the [`IoTaskPool`].
+    /// Like the [`IoTaskPool`], this is intended for IO-intensive work.
+    ///
+    /// Given an `async` function that takes a `Sender` this method schedules it on Bevy's
+    /// `TaskPool` and handles polling progress and completion on the main schedule.
+    #[must_use = "async components do nothing unless polled"]
+    pub fn new_io<F, Fut>(task: F) -> Self
+    where
+        F: FnOnce(Sender<CommandQueue>) -> Fut + Send + 'static,
+        Fut: Future<Output = Result<(), BevyError>> + Send + 'static,
+    {
+        let (sender, receiver) = unbounded::<CommandQueue>();
+        let task = IoTaskPool::get().spawn(task(sender));
         AsyncComponent { task, receiver }
     }
 }
