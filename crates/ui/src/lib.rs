b573c863462c12edd01435a1de4027e4aa01092a
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-04 17:38:42 +0200

    [ui] add basic camera controls

--- crates/ui/src/lib.rs
+++ crates/ui/src/lib.rs
@@ -1,5 +1,6 @@
 #![doc = include_str!("../README.md")]
 
+mod camera;
 mod plugin;
 
 pub use plugin::UIPlugin;
--- crates/ui/src/plugin.rs
+++ crates/ui/src/plugin.rs
@@ -1,6 +1,7 @@
 //! Defines the [`UIPlugin`] which inserts all UI related functionality into the bevy `App`.
+use crate::camera::{UICamera, camera_control_system};
 use bevy::app::App;
-use bevy::prelude::Plugin;
+use bevy::prelude::{AssetServer, Commands, Plugin, PostUpdate, Res, Sprite, Startup};
 use bevy_egui::EguiPlugin;
 use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
@@ -9,9 +10,17 @@ pub struct UIPlugin;
 
 impl Plugin for UIPlugin {
     fn build(&self, app: &mut App) {
+        app.add_systems(PostUpdate, camera_control_system);
         app.add_plugins(EguiPlugin {
             enable_multipass_for_primary_context: true,
         })
-        .add_plugins(WorldInspectorPlugin::new());
+        .add_plugins(WorldInspectorPlugin::new())
+        .add_systems(Startup, setup);
     }
+}
+#[utils::bevy_system]
+fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+    commands.spawn(UICamera::bundle());
+
+    commands.spawn(Sprite::from_image(asset_server.load("logo.png")));
 }
