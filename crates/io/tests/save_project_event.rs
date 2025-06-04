2b8f72837485fbc075f19d9762c6f5285dabd90a
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-04 11:29:15 +0200

    [io] expand save project test for complete event

--- crates/io/tests/save_project_event.rs
+++ crates/io/tests/save_project_event.rs
@@ -3,19 +3,21 @@
 #![allow(clippy::pedantic)]
 
 use anyhow::Context;
+use bevy::ecs::system::SystemState;
 use bevy::prelude::*;
 use data::{Layer, Level, Project};
 use io::*;
 use std::time::Duration;
 use std::{fs::read_to_string, path::PathBuf};
 use tempfile::tempdir;
+use utils::CorePlugin;
 
 #[test]
 fn save_project_event() -> anyhow::Result<()> {
     // Holds output files for this test, we hold the variable since it's deleted on drop.
     let temp_dir = tempdir()?;
     let mut app = App::new();
-    app.add_plugins((MinimalPlugins, IOPlugin));
+    app.add_plugins((MinimalPlugins, CorePlugin, IOPlugin));
     app.insert_resource(Time::<Fixed>::from_duration(Duration::from_secs(1)));
     app.world_mut().spawn((
         Project::new("Example Project"),
@@ -51,6 +53,25 @@ fn save_project_event() -> anyhow::Result<()> {
     app.world_mut().run_schedule(FixedPostUpdate);
     app.update();
 
+    let mut system_state: SystemState<EventReader<SaveProjectCompleteEvent>> =
+        SystemState::new(app.world_mut());
+    let mut events = system_state.get_mut(app.world_mut());
+    let event = events.read().next();
+
+    assert!(
+        event.is_some(),
+        "A completed event should have been dispatched"
+    );
+    assert_eq!(
+        event.unwrap().project,
+        project,
+        "The completed event should have been for the project"
+    );
+    assert_eq!(
+        event.unwrap().output,
+        output,
+        "The completed event should have been for the  output"
+    );
     // TODO: this fails to query the event.
     // assert_eq!(
     //     app.world_mut().resource::<Events<SaveProjectCompleteEvent>>().len(),
