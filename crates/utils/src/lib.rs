fa15f4059ff73eed8b14995a9ae82a0d39839e77
Wannes Gennar <wannes.gennar@gmail.com>
2025-06-10 16:14:21 +0200

    [editor, logging, utils] move logging from utils to `logging` crate

--- crates/utils/src/lib.rs
+++ crates/utils/src/lib.rs
@@ -1,10 +1,8 @@
 #![doc = include_str!("../README.md")]
 
 mod async_ecs;
-mod logging;
 mod plugin;
 
 pub use async_ecs::*;
-pub use logging::log_plugin;
 pub use plugin::CorePlugin;
 pub use utils_macros::*;
