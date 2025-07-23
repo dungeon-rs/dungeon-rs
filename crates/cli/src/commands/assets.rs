2172c27f0b16921e0fb7424b88e55d68afce6f9c
Wannes Gennar <wannes.gennar@gmail.com>
2025-07-23 15:57:31 +0200

    refactor: add additional logging

--- crates/cli/src/commands/assets.rs
+++ crates/cli/src/commands/assets.rs
@@ -4,7 +4,7 @@ use anyhow::Context;
 use assets::AssetLibrary;
 use clap::Subcommand;
 use log::{debug, info};
-use std::path::PathBuf;
+use std::path::{Path, PathBuf};
 
 /// Manage asset library and packs
 #[derive(Debug, clap::Args)]
@@ -50,7 +50,7 @@ pub fn execute(Args { command }: Args) -> anyhow::Result<()> {
     match command {
         Commands::List { path } => execute_list(path),
         Commands::CleanUp { path } => execute_cleanup(path),
-        Commands::Add { library, path } => execute_add(library, path),
+        Commands::Add { library, path } => execute_add(library, &path),
     }
 }
 
@@ -58,9 +58,9 @@ pub fn execute(Args { command }: Args) -> anyhow::Result<()> {
 ///
 /// # Errors
 /// Return an error when the asset library fails to load.
-fn execute_list(path : Option<PathBuf>) -> anyhow::Result<()> {
+fn execute_list(path: Option<PathBuf>) -> anyhow::Result<()> {
     debug!("Attempting to load asset library");
-    let library = AssetLibrary::load_or_default(path)?;
+    let library = AssetLibrary::load_or_default(path).context("Failed to load asset library")?;
     for (name, path) in library.iter() {
         println!("{}: {}", name, path.display());
     }
@@ -73,7 +73,8 @@ fn execute_list(path : Option<PathBuf>) -> anyhow::Result<()> {
 /// # Errors
 /// Return an error when the asset library fails to load.
 fn execute_cleanup(path: Option<PathBuf>) -> anyhow::Result<()> {
-    let library = AssetLibrary::load(path)?;
+    debug!("Attempting to load asset library");
+    let library = AssetLibrary::load(path).context("Failed to load asset library")?;
 
     library.delete().context("Failed to delete asset library")
 }
@@ -82,10 +83,19 @@ fn execute_cleanup(path: Option<PathBuf>) -> anyhow::Result<()> {
 ///
 /// # Errors
 /// Return an error when the asset library fails to load.
-fn execute_add(library: Option<PathBuf>, path: PathBuf) -> anyhow::Result<()> {
-    let mut asset_library = AssetLibrary::load_or_default(library.clone())?;
-    let added_pack = asset_library.add_pack(path.as_path(), None)?;
-    asset_library.save(library)?;
+fn execute_add(library: Option<PathBuf>, path: &Path) -> anyhow::Result<()> {
+    debug!("Attempting to load asset library");
+    let mut asset_library =
+        AssetLibrary::load_or_default(library.clone()).context("Failed to load asset library")?;
+
+    let added_pack = asset_library
+        .add_pack(path, None)
+        .context("Failed to add asset pack to asset library")?;
+
+    debug!("Attempting to save asset library");
+    asset_library
+        .save(library)
+        .context("Failed to save asset library")?;
 
     info!("Added {} as '{}' to library", path.display(), added_pack);
     Ok(())
