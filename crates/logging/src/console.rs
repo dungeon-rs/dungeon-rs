2fa8ac65e491e47c9fce95fa879238d8bbd5d8fc
Wannes Gennar <wannes.gennar@gmail.com>
2025-08-11 22:24:47 +0200

    feat: add `indicatif` support for CLI
    - Add `indicatif` support for console applications

---
+++ crates/logging/src/console.rs
@@ -0,0 +1,78 @@
+#![cfg(feature = "console")]
+//! This module contains all functionality for logging integration when running as a console application.
+//!
+//! This ranges from setting up `tracing` to ensuring progress isn't clobbered by logging.
+
+use bevy::log::BoxedLayer;
+pub use indicatif::MultiProgress;
+use std::io;
+use tracing_subscriber::fmt::{MakeWriter, layer};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::{Layer, Registry};
+
+/// This represents the integration between `tracing` and `indicatif`.
+///
+/// This is largely lifted from `tracing-indicatif` but doesn't integrate span tracking.
+#[derive(Clone, Default)]
+struct IndicatifWriter {
+    /// The `MultiProgress` instance used to display progress.
+    /// All progress instances added to this will automatically be suspended when `tracing` writes
+    /// to stdout.
+    pub progress: MultiProgress,
+}
+
+/// Initialises logging for console applications.
+///
+/// # Errors
+/// This method may return errors when initialising the global tracing subscriber fails.
+#[cfg(feature = "console")]
+pub fn console_logging(
+    level: tracing::metadata::LevelFilter,
+) -> Result<MultiProgress, tracing::subscriber::SetGlobalDefaultError> {
+    let console = IndicatifWriter::default();
+
+    let layer = layer()
+        .with_file(false)
+        .with_thread_names(true)
+        .with_thread_ids(true)
+        .with_level(true)
+        .compact()
+        .with_writer(console.clone())
+        .with_filter(level);
+
+    let layer: BoxedLayer = Box::new(vec![layer.boxed()]);
+
+    let subscriber = Registry::default();
+    tracing::dispatcher::set_global_default(subscriber.with(layer).into())?;
+    todo!()
+}
+
+impl<'a> MakeWriter<'a> for IndicatifWriter {
+    type Writer = IndicatifWriter;
+
+    fn make_writer(&'a self) -> Self::Writer {
+        self.clone()
+    }
+}
+
+impl io::Write for IndicatifWriter {
+    fn write(&mut self, buf: &[u8]) -> io::Result<usize> {
+        self.progress.suspend(|| io::stdout().write(buf))
+    }
+
+    fn write_vectored(&mut self, bufs: &[io::IoSlice<'_>]) -> io::Result<usize> {
+        self.progress.suspend(|| io::stdout().write_vectored(bufs))
+    }
+
+    fn flush(&mut self) -> io::Result<()> {
+        self.progress.suspend(|| io::stdout().flush())
+    }
+
+    fn write_all(&mut self, buf: &[u8]) -> io::Result<()> {
+        self.progress.suspend(|| io::stdout().write_all(buf))
+    }
+
+    fn write_fmt(&mut self, fmt: std::fmt::Arguments<'_>) -> io::Result<()> {
+        self.progress.suspend(|| io::stdout().write_fmt(fmt))
+    }
+}
--- crates/logging/src/lib.rs
+++ crates/logging/src/lib.rs
@@ -1,9 +1,14 @@
 #![doc = include_str!("../README.md")]
 
+mod console;
+
 use bevy::log::tracing_subscriber::Layer;
 use bevy::log::tracing_subscriber::fmt::layer;
 use bevy::log::{DEFAULT_FILTER, Level, LogPlugin};
 use std::str::FromStr;
+
+#[cfg(feature = "console")]
+pub use console::*;
 
 /// Builds the [`LogPlugin`] for the application.
 #[must_use]
@@ -36,30 +41,4 @@ pub fn log_plugin(config: &config::LogConfiguration) -> bevy::log::LogPlugin {
             }
         },
     }
-}
-
-/// Initialises logging for console applications.
-///
-/// # Errors
-/// This method may return errors when initialising the global tracing subscriber fails.
-#[cfg(feature = "console")]
-pub fn console_logging(
-    level: tracing::metadata::LevelFilter,
-) -> Result<(), tracing::subscriber::SetGlobalDefaultError> {
-    use bevy::log::BoxedLayer;
-    use tracing_subscriber::Registry;
-    use tracing_subscriber::layer::SubscriberExt;
-
-    let layer = layer()
-        .with_file(false)
-        .with_thread_names(true)
-        .with_thread_ids(true)
-        .with_level(true)
-        .compact()
-        .with_filter(level);
-
-    let layer: BoxedLayer = Box::new(vec![layer.boxed()]);
-
-    let subscriber = Registry::default();
-    tracing::dispatcher::set_global_default(subscriber.with(layer).into())
 }
