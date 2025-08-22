8c5e4f189e5476172de33727d5175bdd70f7abd3
Wannes Gennar <wannes.gennar+github@gmail.com>
2025-08-22 13:54:06 +0200

    refactor(ui): remove clutter
    First refactor pass, removing clutter

--- crates/ui/src/layout/levels.rs
+++
@@ -1,8 +0,0 @@
-//! Contains the rendering logic for the [`crate::layout::EditorPanels::Levels`] pane.
-use crate::layout::EditorLayout;
-use egui::Ui;
-
-/// Handles the rendering of the [`crate::layout::EditorPanels::Levels`] tab.
-pub(super) fn render(_viewer: &mut EditorLayout, ui: &mut Ui) {
-    ui.label("Rendering asset library here");
-}
