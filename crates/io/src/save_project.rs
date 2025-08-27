d74c74fa3cec9784f5e892432b571356ddccbc69
Wannes Gennar <wannes.gennar+github@gmail.com>
2025-08-27 19:13:42 +0200

    feat(io): remove `output` field from `SaveProjectEvent`

--- crates/io/src/save_project.rs
+++ crates/io/src/save_project.rs
@@ -4,7 +4,7 @@ use anyhow::Context;
 use bevy::prelude::{BevyError, Commands, Entity, Event, EventReader, Query, default};
 use data::{ElementQuery, LayerQuery, LevelQuery, ProjectQuery};
 use serialization::serialize_to;
-use std::{fs::File, path::PathBuf};
+use std::fs::File;
 use utils::{AsyncComponent, report_progress};
 
 /// When this event is sent, the associated `project` will be fetched and saved.
@@ -19,8 +19,6 @@ use utils::{AsyncComponent, report_progress};
 pub struct SaveProjectEvent {
     /// The [`Entity`] of the [`data::Project`] to save.
     pub(crate) project: Entity,
-    /// The output path of the savefile that will be created.
-    pub(crate) output: PathBuf,
 }
 
 /// This event indicates that the work of a [`SaveProjectEvent`] has completed.
@@ -28,19 +26,13 @@ pub struct SaveProjectEvent {
 pub struct SaveProjectCompleteEvent {
     /// The [`Entity`] of the [`data::Project`] that was saved.
     pub project: Entity,
-    /// The output path of the savefile that was created.
-    #[allow(
-        dead_code,
-        reason = "Temporarily until editor and status reporting is implemented"
-    )]
-    pub output: PathBuf, // TODO: remove dead_code
 }
 
 impl SaveProjectEvent {
     /// Generate a new [`SaveProjectEvent`] that can be dispatched.
     #[must_use = "This event does nothing unless you dispatch it"]
-    pub fn new(project: Entity, output: PathBuf) -> Self {
-        Self { project, output }
+    pub fn new(project: Entity) -> Self {
+        Self { project }
     }
 }
 
@@ -63,7 +55,7 @@ pub fn handle_save_project(
     let project = project_query.get(event.project)?;
 
     let entity = event.project;
-    let output = event.output.clone();
+    let output = project.project.file.clone();
     let document = Document::new(&project, level_query, layer_query, object_query);
     commands.spawn(AsyncComponent::new_io(
         async move |sender| {
@@ -73,13 +65,7 @@ pub fn handle_save_project(
             serialize_to(&document, &default(), file)?;
 
             // Report completion
-            report_progress(
-                &sender,
-                SaveProjectCompleteEvent {
-                    project: entity,
-                    output,
-                },
-            )?;
+            report_progress(&sender, SaveProjectCompleteEvent { project: entity })?;
             Ok(())
         },
         |_, _| {
--- crates/io/tests/save_project_event.rs
+++ crates/io/tests/save_project_event.rs
@@ -7,8 +7,8 @@ use bevy::ecs::system::SystemState;
 use bevy::prelude::*;
 use data::{Layer, Level, Project};
 use io::*;
+use std::fs::read_to_string;
 use std::time::Duration;
-use std::{fs::read_to_string, path::PathBuf};
 use tempfile::tempdir;
 use utils::CorePlugin;
 
@@ -50,10 +50,13 @@ fn save_project_event() -> anyhow::Result<()> {
     // Holds output files for this test, we hold the variable since it's deleted on drop.
     let temp_dir = tempdir()?;
     let mut app = App::new();
+    let mut output = std::path::PathBuf::from(temp_dir.path());
+    output.push("save_project_event_test_output.json"); // set output filename
+
     app.add_plugins((MinimalPlugins, CorePlugin, IOPlugin));
     app.insert_resource(Time::<Fixed>::from_duration(Duration::from_secs(1)));
     app.world_mut().spawn((
-        Project::new("Example Project"),
+        Project::new(output.clone(), "Example Project"),
         children![(
             Level::new("First Level"),
             children![(Layer::new("First Layer", Transform::IDENTITY), children![])]
@@ -64,13 +67,10 @@ fn save_project_event() -> anyhow::Result<()> {
         .query::<(&Project, Entity)>()
         .single(app.world())?;
 
-    let mut output = PathBuf::from(temp_dir.path());
-    output.push("save_project_event_test_output.json"); // set output filename
     // run the schedules once to process Setup and spawn
     app.update();
 
-    app.world_mut()
-        .send_event(SaveProjectEvent::new(project, output.clone()));
+    app.world_mut().send_event(SaveProjectEvent::new(project));
 
     process_async_components(&mut app);
 
@@ -88,17 +88,6 @@ fn save_project_event() -> anyhow::Result<()> {
         project,
         "The completed event should have been for the project"
     );
-    assert_eq!(
-        event.unwrap().output,
-        output,
-        "The completed event should have been for the  output"
-    );
-    // TODO: this fails to query the event.
-    // assert_eq!(
-    //     app.world_mut().resource::<Events<SaveProjectCompleteEvent>>().len(),
-    //     1,
-    //     "SaveProjectEvent should have been emitted"
-    // );
 
     let json = read_to_string(output.clone())
         .with_context(|| format!("Output file {} could not be opened", output.display()))?;
